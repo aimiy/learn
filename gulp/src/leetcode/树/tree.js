3f4789013738baee155d0dd269a41f70dc52e5fd
fuym <fuym@yuntongxun.com>
2022-03-27 18:52:42 +0800

    以前看的算法忘提交了

--- gulp/src/leetcode/树/tree.js
+++ gulp/src/leetcode/树/tree.js
@@ -1,3 +1,7 @@
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
 let changeTree = {
     arrToObj:function(arr){
         return {}
@@ -9,3 +13,29 @@ let changeTree = {
         return console.log("  4\n / \\\n5   6")
     }
 }
+// let a = new TreeNode(6);
+
+// let b = new TreeNode(5);
+// b.left = a;
+// console.log(a,b)
+
+// let c = {
+//     val: 2,
+//     left: {
+//         val: 7
+//     },
+//     right: {
+//         val: 4
+//     }
+// }
+// let d = {
+//     val: 2,
+//     left: {
+//         val: 7
+//     },
+//     right: {
+//         val: 4
+//     }
+// }
+
+// console.log(a==b.left,c.left==c.left)
