7b0f02e02bb6102c3c9896a5b14edea264b3e38f
fuym <fuym@yuntongxun.com>
2022-04-06 00:24:09 +0800

    vue3+ts

---
+++ ts/vue3+tsc基础组件/genetics.ts
@@ -0,0 +1,6 @@
+function echo(arg: number | string): number | string {
+    return arg
+}
+
+
+const result = echo('123')
---
+++ ts/vue3+tsc基础组件/my-project/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{count}}</h1>
+    <h1>{{double}}</h1>
+    <button @click="increase">👍+1</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, reactive, ref, toRef, toRefs } from "vue";
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+}
+export default defineComponent({
+  name: "App",
+  setup() {
+    // const count = ref(0);
+    // const double = computed(() => count.value*2)
+    // const increase = () => {
+    //   count.value ++
+    // }
+    // return {
+    //   count,
+    //   double,
+    //   increase,
+    // };
+    const data:DataProps = reactive({
+      count: 0,
+      increase: () => {
+        data.count++;
+      },
+      double: computed(() => data.count * 2),
+    });
+    const refData = toRefs(data)
+    console.log(refData)
+    return {
+      ...refData
+    };
+  },
+  components: {},
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
