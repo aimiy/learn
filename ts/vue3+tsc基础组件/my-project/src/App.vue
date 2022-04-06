fe09fc03e970ac98a74b494214b0bc456252df9d
fuym <fuym@yuntongxun.com>
2022-04-06 23:58:03 +0800

    watch

--- ts/vue3+tsc基础组件/my-project/src/App.vue
+++ ts/vue3+tsc基础组件/my-project/src/App.vue
@@ -3,12 +3,21 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>{{count}}</h1>
     <h1>{{double}}</h1>
-    <button @click="increase">👍+1</button>
+    <button @click="increase">👍+1</button><br>
+    <button @click="addGreetings">addGreetings</button>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive, ref, toRef, toRefs } from "vue";
+import {
+  computed,
+  defineComponent,
+  reactive,
+  ref,
+  toRef,
+  toRefs,
+  watch,
+} from "vue";
 interface DataProps {
   count: number;
   double: number;
@@ -22,22 +31,41 @@ export default defineComponent({
     // const increase = () => {
     //   count.value ++
     // }
+    // const greetings = ref('')
+    // watch(greetings,(newValue,oldValue) => {
+    //   console.log('old',oldValue)
+    //   console.log('new',newValue)
+    //   document.title = greetings.value;
+    // })
+    // const addGreetings = () => {
+    //   greetings.value += "hello!"
+    // }
     // return {
     //   count,
     //   double,
     //   increase,
+    //   addGreetings
     // };
-    const data:DataProps = reactive({
+    const data: DataProps = reactive({
       count: 0,
+      greetings: "",
       increase: () => {
         data.count++;
       },
+      addGreetings: () => {
+        greetings.value += "hello!";
+      },
       double: computed(() => data.count * 2),
     });
-    const refData = toRefs(data)
-    console.log(refData)
+    const greetings = ref("");
+    const refData = toRefs(data);
+    watch([greetings, data], (newValue, oldValue) => {
+      console.log("old", oldValue);
+      console.log("new", newValue);
+      document.title = greetings.value + data.count;
+    });
     return {
-      ...refData
+      ...refData,
     };
   },
   components: {},
