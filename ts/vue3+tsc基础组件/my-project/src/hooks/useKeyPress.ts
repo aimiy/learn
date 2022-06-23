2cb5f8c709c36367b4a1dc7a8eb983ea461786f8
aimiy <mioly@foxmail.com>
2022-06-23 14:50:51 +0800

    按键

--- ts/vue3+tsc基础组件/my-project/src/hooks/useKeyPress.ts
+++ ts/vue3+tsc基础组件/my-project/src/hooks/useKeyPress.ts
@@ -3,19 +3,28 @@ import { onBeforeUnmount, onMounted, reactive, toRefs } from "vue"
 function useKeyPress(key: string) {
     const data = reactive({
         isPress:false,
-        updateClick: (e: KeyboardEvent) => {
+        keydown: (e: KeyboardEvent) => {
             console.log(e)
             if(e.key == key){
                data.isPress = true
                console.log(data.isPress)
             }
+        },
+        keyup: (e: KeyboardEvent) => {
+            console.log(e)
+            if(e.key == key){
+               data.isPress = false
+               console.log(data.isPress)
+            }
         }
     })
     onMounted(() => {
-        document.addEventListener("keydown", data.updateClick);
+        document.addEventListener("keydown", data.keydown);
+        document.addEventListener("keyup", data.keyup);
     })
     onBeforeUnmount(() => {
-        document.removeEventListener("keydown", data.updateClick)
+        document.removeEventListener("keydown", data.keydown)
+        document.removeEventListener("keyup", data.keyup)
     })
     const refsData = toRefs(data)
 
